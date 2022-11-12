3c8c026ba2076578e8f0c015d466ca9c0194a7d6
giannurwana19 <giannurwana19@gmail.com>
2022-11-12 08:02:22 +0700

    fix navbar

--- application/views/partials/navbar.php
+++ application/views/partials/navbar.php
@@ -1,14 +1,14 @@
 <nav class="navbar navbar-expand-lg navbar-dark mb-3" style="background-color: #0097a7;">
 	<div class="container">
-		<a class="navbar-brand" href="#">APP Nilai</a>
+		<a class="navbar-brand" href="/">APP Nilai</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 			<div class="navbar-nav">
-				<a class="nav-link active" href="<?= base_url('mahasiswa') ?>">Mahasiswa</span></a>
-				<a class="nav-link" href="<?= base_url('mata-kuliah') ?>">Mata Kuliah</a>
-				<a class="nav-link" href="<?= base_url('nilai') ?>">Nilai</a>
+				<a class="nav-link <?= $this->uri->segment(1) == 'mahasiswa' ? 'active' : '' ?>" href="<?= base_url('mahasiswa') ?>">Mahasiswa</span></a>
+				<a class="nav-link <?= $this->uri->segment(1) == 'mata-kuliah' ? 'active' : '' ?>" href="<?= base_url('mata-kuliah') ?>">Mata Kuliah</a>
+				<a class="nav-link <?= $this->uri->segment(1) == 'nilai' ? 'active' : '' ?>" href="<?= base_url('nilai') ?>">Nilai</a>
 			</div>
 		</div>
 	</div>
